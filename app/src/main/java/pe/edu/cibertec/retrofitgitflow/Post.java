394741060b3e027ea68300dfa3c477e4682156dd
Christian Escobar <christian.mahlj@gmail.pe>
2019-06-30 12:09:51 -0500

    Version 1

---
+++ app/src/main/java/pe/edu/cibertec/retrofitgitflow/Post.java
@@ -0,0 +1,35 @@
+package pe.edu.cibertec.retrofitgitflow;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+
+    private int userId;
+    private int id;
+    private String title;
+
+    @SerializedName("Body")
+    private String text;
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+
+
+}
+
+
